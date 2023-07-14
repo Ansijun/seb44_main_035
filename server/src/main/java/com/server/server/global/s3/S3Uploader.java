54a8cd110ce53403a01fc6e8b019d1667459bc2d
DESKTOP-V2V8TBE\user <ksy956222@gmail.com>
2023-07-14 15:49:44 +0900

    commit

--- server/src/main/java/com/server/server/global/s3/S3Uploader.java
+++ server/src/main/java/com/server/server/global/s3/S3Uploader.java
@@ -1,13 +1,16 @@
 package com.server.server.global.s3;
 
-import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -16,98 +19,27 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 
-@Component
 @Slf4j
+@RequiredArgsConstructor
+@Service
 public class S3Uploader {
-    @Autowired
-    private AmazonS3Client amazonS3Client;
+
+    private final AmazonS3Client amazonS3Client;
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    /**
-     * 로컬 경로에 저장
-     */
-    public String uploadFileToS3(MultipartFile multipartFile, String filePath) {
-        // MultipartFile -> File 로 변환
-        File uploadFile = null;
-        try {
-            uploadFile = convert(multipartFile)
-                    .orElseThrow(() -> new IllegalArgumentException("[error]: MultipartFile -> 파일 변환 실패"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private final AmazonS3 amazonS3;
 
-        // S3에 저장된 파일 이름
-        String fileName = filePath + "/" + UUID.randomUUID();
+    public String upload(MultipartFile multipartFile) throws IOException {
+        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        //저장되는 파일이름이 중복되지 않기 위해 UUID로 생성한 랜덤 값과 파일이름을 연결하여 S3 업로드
 
-        // s3로 업로드 후 로컬 파일 삭제
-        String uploadImageUrl = putS3(uploadFile, fileName);
-        removeNewFile(uploadFile);
-        return uploadImageUrl;
-    }
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentLength(multipartFile.getInputStream().available());
 
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
 
-    /**
-     * S3로 업로드
-     * @param uploadFile : 업로드할 파일
-     * @param fileName : 업로드할 파일 이름
-     * @return 업로드 경로
-     */
-    public String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
-                CannedAccessControlList.PublicRead));
-        return amazonS3Client.getUrl(bucket, fileName).toString();
-    }
-
-    /**
-     * S3에 있는 파일 삭제
-     * 영어 파일만 삭제 가능 -> 한글 이름 파일은 안됨
-     */
-    public void deleteS3(String filePath) throws Exception {
-        try{
-            String key = filePath.substring(56); // 폴더/파일.확장자
-
-            try {
-                amazonS3Client.deleteObject(bucket, key);
-            } catch (AmazonServiceException e) {
-                log.info(e.getErrorMessage());
-            }
-
-        } catch (Exception exception) {
-            log.info(exception.getMessage());
-        }
-        log.info("[S3Uploader] : S3에 있는 파일 삭제");
-    }
-
-    /**
-     * 로컬에 저장된 파일 지우기
-     * @param targetFile : 저장된 파일
-     */
-    private void removeNewFile(File targetFile) {
-        if (targetFile.delete()) {
-            log.info("[파일 업로드] : 파일 삭제 성공");
-            return;
-        }
-        log.info("[파일 업로드] : 파일 삭제 실패");
-    }
-
-    /**
-     * 로컬에 파일 업로드 및 변환
-     * @param file : 업로드할 파일
-     */
-    private Optional<File> convert(MultipartFile file) throws IOException {
-        // 로컬에서 저장할 파일 경로 : user.dir => 현재 디렉토리 기준
-        String dirPath = System.getProperty("user.dir") + "/" + file.getOriginalFilename();
-        File convertFile = new File(dirPath);
-
-        if (convertFile.createNewFile()) {
-            // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-
-        return Optional.empty();
+        return amazonS3.getUrl(bucket, s3FileName).toString();
     }
 }
